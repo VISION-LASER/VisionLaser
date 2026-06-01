6ad2d21f1c64d5b22e74fd789b6dc4f9ab9b3136
andritina <tonny.andritina@gmail.com>
2026-06-01 14:40:22 +0300

    DESIGN

--- src/components/user/DifferentesEtapes/DifferentesEtapesSection.tsx
+++ src/components/user/DifferentesEtapes/DifferentesEtapesSection.tsx
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 import img1 from "../../../assets/1.jpg";
 import img2 from "../../../assets/2.jpg";
 import img3 from "../../../assets/3.jpeg";
-import img4 from "../../../assets/profit_view.png";
+import img4 from "../../../assets/4.jpeg";
 
 const DifferentesEtapesSection: React.FC = () => {
   const steps = [
